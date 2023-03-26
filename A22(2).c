8e18149eebc217dc6f0b23994b4faea1b9f90837
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-26 15:26:50 +0530

    Add files via upload

---
+++ A22(2).c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int *ptr,c;
+    int i=0,size = 0,sum=0;
+
+    printf("Enter size of array :");
+    scanf("%d",&size);
+
+    ptr =(int*)calloc(size,sizeof(int));
+
+    if(ptr==NULL)
+    {
+        printf("Memory Allocation Failed \n");
+        return 0;
+    }
+    printf("\n The entered %d value \n",size);
+    for(i=0;i<size;i++)
+    {
+      scanf("%d",ptr+i);
+    }
+
+    for(i=0;i<size;i++)
+    {
+      sum = sum + *(ptr+i);
+    }
+
+    printf("Aversge is %d \n",sum/size);
+    free(ptr);
+
+    return 0;
+
+}
